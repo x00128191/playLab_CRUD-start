477e5fc1a904dea1cfc9e7e736d9e3da59ab39d2
x00128191 <x00128191@gmail.com>
2016-11-10 12:20:20 +0000

    Lab8Finished

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,6 +1,16 @@
 package controllers;
+import play.api.Environment;
 import play.mvc.*;
+import play.data.*;
+import play.db.ebean.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+
 import views.html.*;
+
+import models.*;
 
 public class HomeController extends Controller {
 
@@ -13,8 +23,13 @@ public class HomeController extends Controller {
     }
 
     public Result products() {
-        return ok(products.render());
+
+	List<Product> productsList = Product.findAll();	
+
+        return ok(products.render(productsList));
     }
+
+    
 
 }
 
